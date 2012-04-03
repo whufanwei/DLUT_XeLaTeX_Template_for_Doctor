89cf50091633fed9f621b6f903cedfa8d0e923c1
whufanwei <whufanwei@gmail.com>
2012-04-04 03:54:02 +0800

    大连理工博士LaTeX模板 0.8

---
+++ auto/main.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook "main"
+ (lambda ()
+    (LaTeX-add-bibliographies
+     "body/reference")
+    (TeX-run-style-hooks
+     "latex2e"
+     "bk12"
+     "book"
+     "12pt"
+     "a4paper"
+     "openany"
+     "twoside"
+     "setup/fonts"
+     "setup/packages"
+     "setup/format"
+     "preface/cover"
+     "body/chap00"
+     "body/chap01"
+     "body/chap02"
+     "body/chap03"
+     "body/conclusion"
+     "appendix/chapA"
+     "appendix/publications"
+     "appendix/acknowledgements"
+     "appendix/about")))
+
